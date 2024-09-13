9db375386718ce4982371a3b894d8e46b47ccba6
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-09-14 00:41:30 +0800

    toggle mode

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -39,7 +39,7 @@ const NavBar = () => {
           <li>
             <button className="toggle-button-theme" onClick={toggleDarkMode}>
               {isDarkMode ?
-                <svg xmlns="http://www.w3.org/2000/svg" id="moon-svg" width="1.5rem" height="1.5rem" fill="black">
+                <svg xmlns="http://www.w3.org/2000/svg" id="moon-svg" width="1.5rem" height="1.5rem" fill="white">
                   <path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 
                   0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243
                   0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 
