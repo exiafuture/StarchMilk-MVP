229210dbae06cfd73dc8f78933dd824ffa070caf
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-09-15 00:56:51 +0800

    toggle mode fk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "./components/navbar";
+import ThemeProviderWrapper from "./components/themeWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -29,8 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        {children}
+        <ThemeProviderWrapper>
+          <NavBar />
+          {children}
+        </ThemeProviderWrapper>
       </body>
     </html>
   );
